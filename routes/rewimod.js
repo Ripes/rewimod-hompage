1cc4dc450e10ed628b3e1b335963efa9c969b355
Markus Heinemann <ripehostess@gmail.com>
2016-08-04 14:58:27 +0200

    Add Join Page

--- routes/rewimod.js
+++ routes/rewimod.js
@@ -35,4 +35,21 @@ router.get('/download/:version', function (req, res) {
     // TODO: Insert Code for RSS Reader from Jenkins @Sofaklient
 });
 
+router.get('/join', function (req, res) {
+    fs.readFile(path.join(__dirname, '/../public/static/join.html'), 'utf8', function (err, text) {
+        if (err) {
+            throw err;
+        }
+        res.send(text);
+    });
+});
+
+router.get('/legal', function (req, res) {
+    fs.readFile(path.join(__dirname, '/../public/static/legal.html'), 'utf8', function (err, text) {
+        if (err) {
+            throw err;
+        }
+        res.send(text);
+    });
+});
 module.exports = router;
